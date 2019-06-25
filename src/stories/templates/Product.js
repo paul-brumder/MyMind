72a873b0ea2e23c533702bcc454a9e5bfd226a87
paul.brumder <paul.brumder@gmail.com>
2019-06-25 23:56:17 +0200

    New template : Product (temporary)

---
+++ src/stories/templates/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import { CSSTransition } from 'react-transition-group';
+
+const Wrapper = styled.div``;
+
+const Product = () => {
+  return (
+    <Wrapper>
+    </Wrapper>
+  );
+};
+
+export default Product;
