4b5a7593323f50227c362a327e5ea819a9d020ba
paul.brumder <paul.brumder@gmail.com>
2019-08-07 15:22:05 +0200

    UseEffect added + rendering bug fixed

--- src/stories/animations/ImageWave.js
+++ src/stories/animations/ImageWave.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import * as PIXI from 'pixi.js';
 
@@ -11,68 +11,70 @@ const ImageWave = () => {
   document.body.appendChild(renderer.view);
 
   // load assets
-  loader
-    .add('https://i.imgur.com/M0SKsEE.jpg')
-    .add('https://www.fariskassim.com/stage/learn/pixi/circ_ripple/map.png')
-    .load(setup);
+  useEffect(() => {
+    loader
+      .add('https://i.imgur.com/M0SKsEE.jpg')
+      .add('https://www.fariskassim.com/stage/learn/pixi/circ_ripple/map.png')
+      .load(setup);
 
-  var ripples = [];
+    const ripples = [];
 
-  function setup() {
-    // background
-    var bg = new PIXI.Sprite(
-      loader.resources['https://i.imgur.com/M0SKsEE.jpg'].texture
-    );
-    bg.anchor.set(0.5);
-    bg.scale.set(0.6);
-    bg.position.set(renderer.view.width / 2, renderer.view.height / 2);
-    stage.addChild(bg);
+    function setup() {
+      // background
+      const bg = new PIXI.Sprite(
+        loader.resources['https://i.imgur.com/M0SKsEE.jpg'].texture
+      );
+      bg.anchor.set(0.5);
+      bg.scale.set(0.6);
+      bg.position.set(renderer.view.width / 2, renderer.view.height / 2);
+      stage.addChild(bg);
 
-    // New ripple each time mouseenter
-    renderer.view.addEventListener(
-      'mouseenter',
-      function(ev) {
-        ripples.push(new Ripple(ev.clientX, ev.clientY));
-        stage.filters = ripples.map(function(f) {
-          return f.filter;
-        });
-      },
-      false
-    );
+      // New ripple each time mouseenter
+      renderer.view.addEventListener(
+        'mouseenter',
+        function(ev) {
+          ripples.push(new Ripple(ev.clientX, ev.clientY));
+          stage.filters = ripples.map(function(f) {
+            return f.filter;
+          });
+        },
+        false
+      );
 
-    gameLoop();
-  }
-
-  function gameLoop() {
-    requestAnimationFrame(gameLoop);
-
-    // update ripples
-    for (var i = 0; i < ripples.length; i++) {
-      ripples[i].update();
+      gameLoop();
     }
 
-    renderer.render(stage);
-  }
+    function gameLoop() {
+      requestAnimationFrame(gameLoop);
 
-  function Ripple(x, y) {
-    // sprite
-    this.sprite = new PIXI.Sprite(
-      loader.resources[
-        'https://www.fariskassim.com/stage/learn/pixi/circ_ripple/map.png'
-      ].texture
-    );
-    this.sprite.anchor.set(0.5);
-    this.sprite.position.set(x, y);
-    this.sprite.scale.set(0.1);
-    stage.addChild(this.sprite);
-    // filter
-    this.filter = new PIXI.filters.DisplacementFilter(this.sprite);
-  }
+      // update ripples
+      for (let i = 0; i < ripples.length; i++) {
+        ripples[i].update();
+      }
 
-  Ripple.prototype.update = function() {
-    this.sprite.scale.x += 0.025;
-    this.sprite.scale.y += 0.025;
-  };
+      renderer.render(stage);
+    }
+
+    function Ripple(x, y) {
+      // sprite
+      this.sprite = new PIXI.Sprite(
+        loader.resources[
+          'https://www.fariskassim.com/stage/learn/pixi/circ_ripple/map.png'
+        ].texture
+      );
+      this.sprite.anchor.set(0.5);
+      this.sprite.position.set(x, y);
+      this.sprite.scale.set(0.1);
+      stage.addChild(this.sprite);
+      // filter
+      this.filter = new PIXI.filters.DisplacementFilter(this.sprite);
+    }
+
+    Ripple.prototype.update = function() {
+      this.sprite.scale.x += 0.025;
+      this.sprite.scale.y += 0.025;
+    };
+  }, [loader, renderer, stage]);
   return <Wrapper />;
 };
 
